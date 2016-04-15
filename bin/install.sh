aab9154aede25059a687f6d12eceee48c2a8c479
Vasyl Lytovchenko <muaddeep@gmail.com>
2016-04-16 00:12:17 +0300

    user rsync for copying(if exists)

--- bin/install.sh
+++ bin/install.sh
@@ -3,13 +3,18 @@
 CURRENT_DIR=$(pwd)
 CONFIG_DIR=$CURRENT_DIR/configs
 
+CP_COMMAND="cp -r"
+if rsync -h  > /dev/null 2>&1; then
+    CP_COMMAND="rsync -r"
+fi
+
 if [ ! -d "$CONFIG_DIR" ]; then
     echo "$CONFIG_DIR - no such file or directory"
     exit
 fi
 
 ls $CONFIG_DIR | while read i; do
-    echo "copying $CONFIG_DIR/$i to $HOME/.$i";
-    cp $CONFIG_DIR/$i $HOME/.$i
+    echo $CP_COMMAND $CONFIG_DIR/$i $HOME/.$i
+    $CP_COMMAND $CONFIG_DIR/$i $HOME/.$i
 done
 
